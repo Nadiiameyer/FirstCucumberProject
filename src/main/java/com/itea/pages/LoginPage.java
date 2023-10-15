01b97e986446d66b5eb35361667aa9e91fe9e723
Nadiiameyer <nadia.suworowa@hotmail.com>
2023-10-15 11:23:28 +0200

    My final ITEA project

--- src/main/java/com/itea/pages/LoginPage.java
+++ src/main/java/com/itea/pages/LoginPage.java
@@ -1,2 +1,57 @@
-package com.itea.pages;public class LoginPage {
+package com.itea.pages;
+
+import dev.failsafe.internal.util.Assert;
+import io.cucumber.datatable.DataTable;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+import java.util.Map;
+
+public class LoginPage extends BasePage {
+    public LoginPage(WebDriver driver) {
+
+        super(driver);
+    }
+
+    @FindBy(className = "account-text")
+    WebElement loginButton;
+
+    public LoginPage clickOnLoginButton() {
+        click(loginButton);
+        return this;
+    }
+
+    @FindBy(css = "[placeholder='Введіть номер або email']")
+    WebElement emailField;
+    @FindBy(css = "[placeholder='Введіть пароль']")
+    WebElement passwordField;
+
+    public LoginPage enterData(String email, String password) {
+        type(emailField, email);
+        type(passwordField, password);
+        return this;
+    }
+    @FindBy(xpath = "//button[contains(text(), 'Вийти')]")
+    WebElement signOutButton;
+
+    public LoginPage isSignOutButtonDisplayed() {
+        isElementDisplayed(signOutButton);
+        return this;
+    }
+
+    public LoginPage enterWrongPassword(DataTable table) {
+        List<Map<String, String>> dataTable = table.asMaps();
+        String email = dataTable.get(0).get("email");
+        String password = dataTable.get(0).get("password");
+
+        enterData(email, password);
+        return this;
+    }
+
+    public LoginPage isAlertAppers() {
+
+        return this;
+    }
 }
--- src/test/java/com/itea/TestRunner.java
+++ src/test/java/com/itea/TestRunner.java
@@ -1,2 +1,13 @@
-package com.itea;public class TestRunner {
+package com.itea;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "/Users/didihenkelmann/Desktop/ITEA/FirstCucumberProject/src/main/resources/features",
+        glue = "com/itea/stepDefinitions",
+        tags = "@validData",
+        plugin = {"pretty", "json:build/cucumber-report/cucumber.java"})
+public class TestRunner {
 }
